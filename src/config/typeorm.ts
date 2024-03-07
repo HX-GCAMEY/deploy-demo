aebd7efb1368fde377817cb908af38f330c20868
Gama <lic.camey@gmail.com>
2024-03-07 17:02:29 +0000

    Update db config

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -8,7 +8,7 @@ dotenvConfig({ path: '.development.env' });
 const config = {
   type: 'postgres',
   host: `${process.env.DB_HOST}` || 'postgresdb',
-  port: 5432,
+  port: `${process.env.DB_PORT}` ||5432,
   username: `${process.env.DB_USER}`,
   password: `${process.env.DB_PASSWORD}`,
   database: `${process.env.DB_NAME}`,
